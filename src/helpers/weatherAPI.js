2a4561f00d7808b0838c225d340bd2d077dbe23e
Erick Henrique <erick.tatui@hotmail.com>
2023-05-25 16:22:29 -0300

    Requisito 01

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,13 @@
 // Remova os comentários a medida que for implementando as funções
+const token = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui start
+export const searchCities = async (term) => {
+  const result = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${term}`);
+  const data = await result.json();
+  if (data.length === 0) {
+    window.alert('Nenhuma cidade encontrada');
+    return data;
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
